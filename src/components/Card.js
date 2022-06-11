23eb9e50ca7a958df2a92cd9add2649311d8863a
naveed-shoukat <shoukat.naveed@gmail.com>
2022-06-11 22:57:34 +0300

    memsdata file add and respective URL shown as background

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import '../Style/Card.css';
-// import memeImg from '../Assets/memeimg.png';
+import memeData from '../Assets/memesData';
 
 export default function Card() {
+  const memes = memeData.data.memes;
+  const [imgURL, setImgURL] = React.useState(memes[5].url);
+
+  function memeImgHandler() {
+    const randumIndex = Math.floor(Math.random(memes.length) * memes.length);
+    console.log(randumIndex);
+    setImgURL(memes[randumIndex].url);
+  }
+  const bgURL = {
+    background: `url(${imgURL}) `,
+  };
+
   return (
     <div className="card">
       <div className="card--inputs">
@@ -20,9 +32,13 @@ export default function Card() {
           placeholder="and take my money"
         />
       </div>
-      <button className="getImg-btn">Get a new meme image 🖼</button>
-      <div className="memeImg"></div>
-      {/* <img className="memeImg" src={memeImg} alt="Meme cartoon" /> */}
+      <button onClick={memeImgHandler} className="getImg-btn">
+        Get a new meme image 🖼
+      </button>
+      <div className="memeImg" style={bgURL}>
+        <h1 className="img-text img-text-top">Shut up</h1>
+        <h1 className="img-text img-text-botom">and take my money</h1>
+      </div>
     </div>
   );
 }
